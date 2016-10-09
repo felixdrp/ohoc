9ec4197b7b6386f52e645162b4b2a8b398e8aacc
Jesus Rodriguez <rpsoft@gmail.com>
2016-10-09 17:57:00 +0100

    Adding packages.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default class App extends Component {
           Hello, world!!!
         </h1>
         <span>
-          First commit
+          Adding packages...ss
         </span>
       </div>
     );
