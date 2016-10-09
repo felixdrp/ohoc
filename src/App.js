c065e7a4bc58c1116555b3419533537233cd6c6c
felixdrp <felixdrp@gmail.com>
2016-10-09 12:07:22 +0100

    First commit!

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          Hello, world!!!
+        </h1>
+        <span>
+          First commit
+        </span>
+      </div>
+    );
+  }
+}
